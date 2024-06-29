f27d61fc858b66320e54380ef02efbdce5847f05
WHORYZEN <kanishkshkl@gmail.com>
2024-06-29 20:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function shortcut(str1, str2) {
     if (str1 === "" || str2 === "") {
-        return "";  // If either string is empty, return an empty string
+        return "";  
     }
     
-    const initial1 = str1.charAt(0).toUpperCase();  // Get the initial letter of str1 (converted to uppercase)
-    const initial2 = str2.charAt(0).toUpperCase();  // Get the initial letter of str2 (converted to uppercase)
+    const initial1 = str1.charAt(0).toUpperCase();  
+    const initial2 = str2.charAt(0).toUpperCase();  
     
-    return initial1 + initial2;  // Return the concatenation of initial letters
+    return initial1 + initial2;
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+const str1 = prompt("Enter s1:");
+const str2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
